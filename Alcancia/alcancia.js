0da46792dbd9ae7b8b5c17756442d96c4b79d121
ChrisSR0 <ChrisSR0@users.noreply.github.com.>
2025-09-07 19:03:10 -0500

    se agrego un archivo

--- Alcancia/alcancia.js
+++ Alcancia/alcancia.js
@@ -1,6 +1,4 @@
 
-
-
 class Alcancia{
 
     constructor(){
@@ -25,6 +23,12 @@ class Alcancia{
         let totalMonto = this.moneda200 * 200 + this.moneda500 * 500 + this.moneda1000 * 1000;
 
         return totalMonto ;
+    }
+
+    resetearLaAlcancia(){
+        this.moneda200 =0;
+        this.moneda500 =0;
+        this.moneda1000 =0;
     }
 }
 export  default Alcancia;
--- Alcancia/main.js
+++ Alcancia/main.js
@@ -1,2 +1,52 @@
 import Alcancia from "./alcancia.js";
 
+const alcancia1  = new Alcancia();
+
+const $txtMonedas200 = document.getElementById("txtMonedas200");
+const $txtMonedas500 = document.getElementById("txtMonedas500");
+const $txtMonedas1000 = document.getElementById("txtMonedas1000");
+const $txtTotalAlcancia = document.getElementById("txtTotalAlcancia");
+const $btnRomper = document.getElementById("btnRomper");
+
+
+
+
+
+const  actualizarInterfaz  = ()=>{
+    $txtMonedas200.value = alcancia1.moneda200;
+    $txtMonedas500.value = alcancia1.moneda500;
+    $txtMonedas1000.value = alcancia1.moneda1000;
+    $txtTotalAlcancia.value = alcancia1.cantidadTotalAhorrada();
+}
+
+
+document.querySelector("#img200").addEventListener("click",()=>{
+    alcancia1.agruegarMoneda200();
+    actualizarInterfaz();
+})
+
+document.querySelector("#img500").addEventListener("click",()=>{
+    alcancia1.agruegarMoneda500();
+    actualizarInterfaz();
+})
+
+document.querySelector("#img1000").addEventListener("click",()=>{
+    alcancia1.agruegarMoneda1000();
+    actualizarInterfaz();
+})
+
+
+function inicializarAlcancia(){
+    alcancia1.resetearLaAlcancia();
+    actualizarInterfaz();
+}
+
+
+$btnRomper.addEventListener("click", inicializarAlcancia);
+
+
+
+
+
+
+
